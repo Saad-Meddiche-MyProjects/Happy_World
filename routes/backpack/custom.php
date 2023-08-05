1e87baa5436b93663d606530c508e82a5b1966a3
S-A-3-D-O-U-N-E <112890463+saadMeddiche@users.noreply.github.com>
2023-08-05 18:20:50 +0100

    make migration for creating servers table

--- routes/backpack/custom.php
+++ routes/backpack/custom.php
@@ -17,4 +17,5 @@ Route::group([
     'namespace'  => 'App\Http\Controllers\Admin',
 ], function () { // custom admin routes
     Route::crud('user', 'UserCrudController');
+    Route::crud('server', 'ServerCrudController');
 }); // this should be the absolute last line of this file
